9fd79ab76ea143e8d6ad3605e1d5b9985e8d2c73
Daniil Mukhreev <muxreev.dan2@yandex.ru>
2021-11-04 15:50:07 +0300

    редактированно

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,35 @@
-console.log("yes");
+// Задание 1
+
+console.log("кот">"код");// true так как т "больше"
+
+console.log("2"+2*"2");//24 2*2=4 "2"+4=24
+
+console.log(undefined==null);/// true
+
+console.log(undefined===null);/// false
+
+console.log(null==0);/// false 
+
+console.log(2>"3");/// false 2<3
+
+console.log(null-false+true);/// 0-0+1=1
+
+console.log(1/"1");//// 1/1=1
+
+console.log("2"*"3");/////// 2*3=6
+
+console.log(4+5+"O");//////9+"o"=9o
+
+console.log("1"+4+5);//// 145
+
+console.log("4"-2);///2
+
+console.log("4"-"4x");/////NaN
+
+console.log(null==false);///false
+
+console.log("-4"/0+1);/// infinity 
+
+console.log(null+1); ////1
+
+console.log(undefined+null);//// NaN
