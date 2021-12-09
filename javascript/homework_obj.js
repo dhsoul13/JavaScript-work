0c9568aa863bd651b3197bb82fa96848fd7ecd8f
Daniil Mukhreev <muxreev.dan2@yandex.ru>
2021-12-09 11:57:55 +0300

    отправленно

--- javascript/homework_obj.js
+++ javascript/homework_obj.js
@@ -334,15 +334,18 @@ console.log(getNumber(777588,getDigitsSum));
 const user= {
     userName:"Даниил",
     userAge:21,
+    userCounty:"NN",
 };
 
-const without=(obj, fild)=>{
+const without=(obj, ...fild)=>{
     let newObj =Object.assign({}, obj);
-    delete newObj[fild];
+    for( let i=0; i<fild.length; i++){
+    delete newObj[fild[i]];
+    };
     return newObj
 }
 
-let userWithout = without(user, "userAge");
+let userWithout = without(user, "userAge","userCounty");
 
 console.log(user);
 console.log(userWithout);
@@ -373,12 +376,371 @@ const objDay = {
     5:"Пятница",
     6:"Суббота",
     7:"Воскресенье",
+
+    day: function(numberDay=1){
+        return this[numberDay];
+    }
 }
 
-const dayPrint=(obj,number)=>{
-    return (obj[number]===undefined) ? "ошибка" : obj[number];
+
+console.log(objDay.day(5));
+
+
+
+// 1. Используя метод map() напишите код, который получает из массива строк новый массив, содержащий их длины.
+
+
+let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка']
+
+const vegetableLength = (mas)=>{
+    return mas.map((el)=>{
+        return el.length;
+    })
+}
+
+console.log("Ответ на 1:",vegetableLength(vegetables));
+
+//Имеется массив простых чисел: numbers = [2, 3, 5, 7, 11, 13, 17, 19]. Использую метод reduce() напишите функцию currentSums(numbers), которая возвращает новый массив из такого же числа элементов, в котором на каждой позиции будет находиться сумма элементов массива numbers до этой позиции включительно.
+
+let numbers = [2, 3, 5, 7, 11, 13, 17];
+
+console.log(numbers);
+
+const  currentSums = (mas) =>{
+
+    return mas.reduce((perValue, el , index) =>{
+
+        if ( index === 0 ){
+             perValue.push(el);
+        } else {
+            let sum = 0;
+            console.log(index);
+            for(let i=0; i<=index;i++){
+                
+                console.log(`индекс ${i}, значение ${mas[i]}`);
+
+                sum = sum + mas[i];
+            }
+            console.log("cумма:", sum);
+            perValue.push(sum);
+            
+        }
+
+        return perValue
+
+    }, []);
+}
+
+console.log(currentSums(numbers));
+
+// 3 Напишите код, который получает из массива чисел новый массив, содержащий пары чисел, которые в сумме должны быть равны семи: (0:7), (1:6) и т.д.
+
+// let arr = [0, 1, 2, 3, 4, 5, 6, 7];
+
+// const sumSeven = (mas)=> {
+
+//     let newArray = [];
+
+//     console.log(newArray);
+
+//     for(let i=0; i < mas.length; i++){
+
+        
+       
+
+//         console.log(newArray);
+//     }
+
+// }
+
+
+// sumSeven(arr)
+
+// 4. Перед вами переменная, содержащая строку. Напишите код, создащий массив, который будет состоять из первых букв слов строки str. 
+
+let strPut = "Каждый охотник желает знать"; 
+
+// Ваш код 
+// const fooStr = (str)=>{
+//     let newArray = [];
+//     str = str.split(" ");
+//     for (let i = 0; i<str.length; i++){
+//         newArray.push(str[i][0]);
+//     };
+//     return newArray
+// }
+
+// console.log(fooStr(strPut));  // [К,о,ж,з,г,с,ф]
+
+
+const fooStr = (str)=>{
+    let newArray = 0;
+    str = str.split(" ");
+    newArray = str.map((el)=>{ return el[0];});
+    return newArray;
 };
 
-console.log(dayPrint(objDay, 5));
+console.log(fooStr(strPut));
+
+// 5 Перед вами переменная, содержащая строку. Напишите код, создащий массив, который будет состоять из строк, состоящих из предыдущего, текущего и следующего символа строки str. 
 
 
+
+// 7. Напишите код, объединяющий три массива цифр, и располагающий цифры, в полученном массиве, в порядке убывания их значений
+
+let a = [1,2,3];
+let b = [4,5,6];
+let c = [7,8,9];
+
+const getArr = (a, b, c)=>{
+    let newMas = [];
+    newMas = newMas.concat(a,b,c);
+    newMas.sort((a,b)=>{return b-a});
+    return newMas;
+}
+
+console.log(getArr(a,b,c));
+
+// 6 Напишите код, преобразующий массив цифр, которые располагаются неупорядоченно, в массив цифр расположенных по убыванию их значений
+
+let numerics = [5, 7, 2, 9, 3, 1, 8];
+
+const foo = (numerics) =>{
+    return numerics.sort((a,b)=>{return b-a});
+}
+
+console.log(foo(numerics));
+
+// 8 Дан двухмерный массив с числами, например [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным. Показать решение.
+
+// let sumElemFromMatrix = 0;
+let matrix= [[1, 2, 3], [4, 5], [6]]
+// for (let innerMatrix of matrix){
+//     for (let value of innerMatrix){
+//         sumElemFromMatrix+=value;
+//     }
+// }
+let sumElemFromMatrix = (matrix) =>{
+    return matrix.flat().reduce((acc,el)=>{
+        return acc+el;
+    });
+};
+
+console.log(sumElemFromMatrix(matrix));
+
+//9. Дан трехмерный массив с числами, например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. Найдите сумму элементов этого массива. Массив, конечно же, может быть произвольным.
+
+let matrixTwo = [
+    [
+        [1, 2], [3, 4]
+    ], 
+    [
+        [5, 6], [7, 8]
+    ]
+]
+
+let sumElemFromMatrixTwo = 0;
+
+for (let innerInnerMatrix of matrixTwo){
+    for (let innerMatrix of innerInnerMatrix){
+        for (let value of innerMatrix ){
+            sumElemFromMatrixTwo+= value
+        }
+    }
+}
+
+console.log(sumElemFromMatrixTwo);
+
+// 10 Дан массив с числами. Не используя метода reverse переверните его элементы в обратном порядке.
+
+
+let numbersDop = [2,3,5,6,7];
+
+const reverseMas = ((mas)=> {
+    let k=mas.length-1;
+    console.log("длина:",mas.length);
+    let element = 0;
+
+    for (let i = 0; i<mas.length/2; i++){
+
+        element=mas[i];
+        mas[i] = mas[k-i];
+        mas[k-i]= element;
+
+    };
+    return mas
+});
+
+let result = reverseMas(numbersDop);
+
+console.log(result);
+
+// 11 Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+
+
+
+// 12 Сделайте функцию arrayFill, которая будет заполнять массив заданными значениями. Первым параметром функция принимает значение, которым заполнять массив, а вторым - сколько элементов должно быть в массиве. Пример: arrayFill('x', 5) сделает массив ['x', 'x', 'x', 'x', 'x'].
+
+
+const arrayFill = (elem, col)=>{
+    let newArray = [];
+    for(let i=0; i<col; i++){ newArray.push(elem); }
+    return newArray;
+};
+console.log(arrayFill("x", 5))
+
+
+
+//13 Создайте объект и скопируйте данный объект с помощью: Object.assign() и spread оператора. Изменить любое свойство в копии объекта, и проверить не изменился ли исходный.
+
+let objPerson = {
+    personName : "Даниил",
+    personAge : "21",
+    personEducation: "НГПУ. им Козьмы Минина",
+}
+
+let clone1 = Object.assign({},objPerson,);
+
+let clone2 = {...objPerson};
+
+clone1.personName = "Александр";
+
+clone2.personName = "Кирилл";
+
+console.log(objPerson);
+console.log(clone1);
+console.log(clone2);
+
+// 14 Создайте функцию, которая принимает 1 аргумент - объект пользователя user со свойствами name, age, city. Функция должна возвращать новый объект пользователя с измененным полем name, при этом оригинальный объект user должен остаться неизменным.
+
+userOne = {
+    userName:"Даниил",
+    userAge:20,
+    userCity:"Нижний Новгород",
+};
+
+
+const newUser = (user)=>{
+    let newOject = Object.assign({}, user);
+    newOject.userName = "Александр";
+    return newOject;
+}
+
+userTwo = newUser(userOne);
+console.log(userOne);
+console.log(userTwo);
+
+//15 Написать функцию которая будет принимать n-ое количество аргументов, в качестве результата функция будет возвращать сумму всех четных элементов. Для решения использовать цикл for (... of ...). 
+
+
+// const sumNElem = (...n)=>{
+//     return n.reduce((acc,el)=>{
+//         return (el % 2 == 0) ? acc + el : acc;
+//     },0)
+// }
+
+const sumNElem = (...n)=>{
+    let sum = 0;
+    for (let value of n){
+        if( value%2 == 0 && value !=0){
+            sum+=value;
+        }
+    }
+    return sum;
+}
+console.log(sumNElem(2, 2 ,4 ,5 ,3))
+
+// 16 Написать функцию, которая принимает слово и возвращает true, если слово является палиндромом.
+
+let word = "ротор";
+
+let checkPalindrome = (str)=>{
+  let mas = [];
+  for  (let i = 0; i<str.length; i++ ){
+      mas.push(str[i]);
+  };
+
+  for (let i= 0; i<mas.length; i++){
+      if (mas[i] != mas.reverse()[i]){
+        return false;
+      }
+  }
+  return true;
+}
+
+console.log(checkPalindrome(word));
+
+//17 Написать функцию которая будет принимать два массива, и в качестве
+// результата будет возвращать только те значения которые есть и в первом и во втором массиве.
+
+
+let mas1 = [1,8,3,7];
+
+let mas2 = [3,7,3,2];
+
+const  selectMas = (arr1, arr2)=>{
+    let newArray = [];
+    for(let i = 0; i< arr1.length; i++){
+        if(arr2.includes(arr1[i])){
+            newArray.push(arr1[i]);
+        }
+    }
+    return newArray
+}
+
+console.log(selectMas(mas1, mas2));
+
+// 18 Дан массив с числами. Запишите в новый массив только те числа, которые больше нуля и меньше 10-ти. Для этого используйте вспомогательную функцию isNumberInRange из предыдущей задачи( Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.).
+
+let mas = [4,2,3,1,52,5552,31,13,53,41,324];
+
+const isNumberInRange = (arr) => {
+    return mas.filter((el)=>{
+        return (el>0 && el<10) ? el : "";
+    })
+}
+
+console.log(isNumberInRange(mas));
+
+//19 делайте функцию getDigitsSum (digit - это цифра), которая параметром принимает целое число и возвращает сумму его цифр.
+
+let number19 = 99;
+
+const getDigitsSumV2 = (el)=>{
+    let arr = el.toString().split('');
+    return arr.reduce((acc, el)=>{
+        return acc+Number(el);
+    },0)
+}
+
+console.log(getDigitsSumV2(number19));
+
+//20 Найдите все года от 1 до 2020, сумма цифр которых равна 13. Для этого используйте вспомогательную функцию getDigitsSum из предыдущей задачи.
+
+const findSumYear = (callback) =>{
+    let sum = 0;
+    for(let i=1; i < 2020; i++){
+        if(callback(i) == 13){
+            sum+=1;
+        };
+    };
+    return sum;
+};
+
+console.log(findSumYear(getDigitsSumV2));
+
+//21 Сделайте функцию getDivisors, которая параметром принимает число и возвращает массив его делителей (чисел, на которое делится данное число без остатка).
+
+let number20 = 10;
+
+const getDivisors = (num)=>{
+    let arr = [];
+    for (let i=1; i<=num; i++){
+        if(num % i == 0){
+            arr.push(i);
+        };
+    };
+    return arr;
+}
+
+console.log(getDivisors(number20));
